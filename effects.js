171f47e6b0757bd05c0568236dc7c8e87cd87c6f
João Vitor Schaidt <joaoschaidt@hotmail.com>
2017-06-05 23:24:11 -0300

    Adicionando index, style, js e imagens
    arquivos usados na construções do curriculo

---
+++ effects.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+		//efeito de preenchimento das barras de progresso
+		$(".pt").css("width", "100%");
+		$(".en").css("width", "57%");
+		$(".java").css("width", "40%");
+		$(".html").css("width", "60%");
+		$(".css").css("width", "50%");
+		$(".js").css("width", "30%");
+		$(".android").css("width", "45%");
+		$(".ps").css("width", "20%");
+		$(".mysql").css("width", "35%");
+   
+});
