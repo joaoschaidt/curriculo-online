d2c9ee8082fa78d44bc997a29decf0a216c0f81f
João Vitor Schaidt <joaoschaidt@hotmail.com>
2017-06-08 14:52:10 -0300

    Mudança no css e ajuste em habilidades
    Mudança para seletores mais específicos, alteração na porcentagem das
    barras.

--- effects.js
+++ effects.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
 
 		//efeito de preenchimento das barras de progresso
 		$(".pt").css("width", "100%");
-		$(".en").css("width", "57%");
-		$(".java").css("width", "40%");
-		$(".html").css("width", "60%");
-		$(".css").css("width", "50%");
-		$(".js").css("width", "30%");
+		$(".en").css("width", "50%");
+		$(".java").css("width", "38%");
+		$(".html").css("width", "49%");
+		$(".css").css("width", "42%");
+		$(".js").css("width", "26%");
 		$(".android").css("width", "45%");
 		$(".ps").css("width", "20%");
 		$(".mysql").css("width", "35%");
